50c95124c16c46d832b135dc1a824c3b72d9cd53
premiash@gmail.com <premiash@gmail.com>
2017-04-05 11:59:55 -0400

    changes committed

--- Application.js
+++ Application.js
@@ -1,41 +1,8 @@
 var fs  = require('fs');
 var inquirer = require('inquirer');
-var command = process.argv[2];
-
-flashCard();
+// var command = process.argv[2];
 
 
-function flashCard() {
-	if (command === 'basic'){
-		inquirer.prompt([{
-			name: 'front',
-			message: 'Enter front of card: '
-		},{
-			name: 'back',
-			message: 'Enter back of card: '
-		}]).then(function(answers){
-			var basicFlashCard = new BasicFlashcard(answers.front, answers.back);
-			basicFlashCard.save();
-		});
-	} else if (command === 'cloze'){
-		inquirer.prompt([{
-			name: 'text',
-			message: 'Enter full text: '
-		},{
-			name: 'cloze',
-			message: 'Enter cloze argument: '
-		}]).then(function(answers){
-			var search = answers.text.search(answers.cloze);
-			var substring = answers.text.substring(0,search) + '________' + answers.text.substring(search + answers.cloze.length);
-			var clozeFlashCard = new ClozeFlashcard(substring, answers.cloze);
-			clozeFlashCard.save();
-		});
-	} else if(command === 'show'){
-		showAllCards();
-	} else {
-		console.log('Please enter "basic" or "cloze" to create a flashcard');
-	}
-}
 
 function BasicFlashcard(front, back){
 	this.front = front;
@@ -63,3 +30,58 @@ function showAllCards() {
 		console.log("[" + data + "]");
 	});
 }
+
+
+
+inquirer.prompt([{
+    type: "list",
+    message: "What type of Flash Card do you like to make?",
+    choices: ["Basic flashcards", "Cloze-Deleted flashcards"],
+    default: ["Basic flashcards"],
+    name: "commands"
+
+}]).then(function(user) {
+
+fs.appendFile("cards.txt", "\n" + user.commands + "\n");
+
+console.log("Generate" + user.commands);
+
+flashCard();
+
+function flashCard() {
+	var command = user.commands;
+
+	if (user.commands === 'Basic flashcards'){
+		inquirer.prompt([{
+			name: 'front',
+			message: 'Enter front of card: '
+		},{
+			name: 'back',
+			message: 'Enter back of card: '
+		}]).then(function(answers){
+			var basicFlashCard = new BasicFlashcard(answers.front, answers.back);
+			basicFlashCard.save();
+		});
+	} else if (user.commands === 'Cloze-Deleted flashcards'){
+		inquirer.prompt([{
+			name: 'text',
+			message: 'Enter full text: '
+		},{
+			name: 'cloze',
+			message: 'Enter cloze argument: '
+		}]).then(function(answers){
+			var search = answers.text.search(answers.cloze);
+			var substring = answers.text.substring(0,search) + '________' + answers.text.substring(search + answers.cloze.length);
+			var clozeFlashCard = new ClozeFlashcard(substring, answers.cloze);
+			clozeFlashCard.save();
+		});
+	} else if(command === 'show'){
+		showAllCards();
+	} else {
+		console.log('Please enter "basic" or "cloze" to create a flashcard');
+	}
+}
+
+});
+
+
